bd8ead70fcb1b6c6cdb15aa92d7555867b624234
Michał Kraciuk <michal.kraciuk@orange.com>
2022-01-15 21:19:21 +0100

    loggers

--- src/main/java/com/akbp/racescore/controller/AuthController.java
+++ src/main/java/com/akbp/racescore/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.akbp.racescore.controller;
 
 import com.akbp.racescore.model.dto.auth.AuthRequest;
 import com.akbp.racescore.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/auth")
 @CrossOrigin("*")
 public class AuthController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
     @Autowired
     private AuthService authService;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@RequestBody AuthRequest request) {
-        return authService.authenticateUser(request);
+        try {
+            return authService.authenticateUser(request);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody AuthRequest signupRequest) {
-        return authService.registerUser(signupRequest);
+        try {
+            return authService.registerUser(signupRequest);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ CREATE SCHEMA IF NOT EXISTS RACE_SCORE;
 CREATE SEQUENCE IF NOT EXISTS race_score.hibernate_sequence START 1;
 
 
-insert into race_score.role values (1, 'USER_ROLE');
-insert into race_score.role values (2, 'ADMIN_ROLE');
-insert into race_score.role values (3, 'EVENT_ADMIN_ROLE');
+--insert into race_score.role values (1, 'USER_ROLE');
+--insert into race_score.role values (2, 'ADMIN_ROLE');
+--insert into race_score.role values (3, 'EVENT_ADMIN_ROLE');
