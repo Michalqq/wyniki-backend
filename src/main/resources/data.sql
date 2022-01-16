5edb3a5e22ef1cbac442584f10a44e7d995ecaa5
Michał Kraciuk <michal.kraciuk@orange.com>
2022-01-16 22:30:30 +0100

    drop table team

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS RACE_SCORE;
 CREATE SEQUENCE IF NOT EXISTS race_score.hibernate_sequence START 1;
 
+alter table race_score.table drop table;
+
 --insert into race_score.role values (1, 'USER_ROLE');
 --insert into race_score.role values (2, 'ADMIN_ROLE');
 --insert into race_score.role values (3, 'EVENT_ADMIN_ROLE');
