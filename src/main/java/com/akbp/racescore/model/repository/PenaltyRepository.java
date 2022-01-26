523dcc7e1f6b27c4b24b762af3e590d33b4711f6
Michał Kraciuk <michal.kraciuk@orange.com>
2022-01-27 00:06:53 +0100

    penalties fix

--- src/main/java/com/akbp/racescore/model/repository/PenaltyRepository.java
+++ src/main/java/com/akbp/racescore/model/repository/PenaltyRepository.java
@@ -12,7 +12,7 @@ public interface PenaltyRepository extends JpaRepository<Penalty, Long> {
 
     @Query(value = "select stage.name, pen.penalty_id penaltyId, pen.description, pen.penalty_kind penaltyKind, pen.penalty_sec penaltySec, et.number, team.driver, team.co_driver coDriver\n" +
             "from race_score.penalty pen " +
-            "left join race_score.event_team et on et.team_id = pen.team_id " +
+            "left join race_score.event_team et on et.team_id = pen.team_id and et.event_id = :eventId" +
             "left join race_score.team team on team.team_id = pen.team_id " +
             "left join race_score.stage stage on stage.stage_id = pen.stage_id " +
             "left join race_score.penalty_dict pd on pd.id = pen.penalty_kind " +
