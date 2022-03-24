da7e4768b67cdd003d9abec90b16039dba38e877
Michał Kraciuk <michal.kraciuk@orange.com>
2022-03-24 23:42:49 +0100

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS RACE_SCORE;
 CREATE SEQUENCE IF NOT EXISTS race_score.hibernate_sequence START 1;
 --
---update race_score.event_team set start_order=number
+update race_score.event_team set start_order=number
+ALTER TABLE  race_score.event ALTER COLUMN description TYPE varchar(1500);
+
 
 --INSERT INTO race_score.penalty_dict values (100, 'Wpisz ręcznie', false, '0');
 
