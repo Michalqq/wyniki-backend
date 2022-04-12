201364f4ca215ed40b9f5337700bbe592a89bd0c
Michał Kraciuk <michal.kraciuk@orange.com>
2022-04-12 21:17:49 +0200

    teamPanel

--- src/main/java/com/akbp/racescore/controller/AppController.java
+++ src/main/java/com/akbp/racescore/controller/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
-    @GetMapping({"/", "/event", "/login", "/register", "/reminder", "/passwordReset", "/message"})
+    @GetMapping({"/", "/event", "/login", "/register", "/reminder", "/passwordReset", "/message", "/teamPanel"})
     public String appPage(Model model) {
         model.addAttribute("version", "v1");
         model.addAttribute("time", "sd");
