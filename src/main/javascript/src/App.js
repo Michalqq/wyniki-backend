45820b4df2f6aee1387a0dbd00abef86eb4d935d
Michał Kraciuk <michal.kraciuk@orange.com>
2022-04-27 21:19:18 +0200

    lazy import

--- src/main/javascript/src/App.js
+++ src/main/javascript/src/App.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AddScorePage from "./components/page/AddScorePage";
 import AddPenaltyPage from "./components/page/AddPenaltyPage";
 import StageScorePage from "./components/page/StageScorePage";
-import HomePage from "./components/page/HomePage";
 import TeamCreatePage from "./components/page/TeamCreatePage";
 import { Footer, NavigationBar } from "./components/common/NavigationBar";
-import LoginPage from "./components/page/auth/LoginPage";
-import RegisterPage from "./components/page/auth/RegisterPage";
-import ReminderPage from "./components/page/auth/ReminderPage";
-import ResetPasswordPage from "./components/page/auth/ResetPasswordPage";
-import MessagePage from "./components/page/MessagePage";
 import { TeamPanel } from "./components/team/TeamPanel";
+import { Spinner } from "react-bootstrap";
+
+const MessagePage = lazy(() => import("./components/page/MessagePage"));
+const LoginPage = lazy(() => import("./components/page/auth/LoginPage"));
+const HomePage = lazy(() => import("./components/page/HomePage"));
+const RegisterPage = lazy(() => import("./components/page/auth/RegisterPage"));
+const ReminderPage = lazy(() => import("./components/page/auth/ReminderPage"));
+const ResetPasswordPage = lazy(() =>
+  import("./components/page/auth/ResetPasswordPage")
+);
 
 function App() {
   return (
@@ -22,27 +26,37 @@ function App() {
 
       <BrowserRouter>
         <NavigationBar />
-        <div className="App container">
-          <div className="shadow bg-body rounded pb-5">
-            <div className="p-0">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/reminder" element={<ReminderPage />} />
-                <Route path="/passwordReset" element={<ResetPasswordPage />} />
-                <Route path="/message" element={<MessagePage />} />
-
-                <Route path="/add_score" element={<AddScorePage />} />
-                <Route path="/add_penalty" element={<AddPenaltyPage />} />
-                <Route path="/event" element={<StageScorePage />} />
-                <Route path="/joinToEvent" element={<TeamCreatePage />} />
-                <Route path="/teamPanel" element={<TeamPanel />} />
-              </Routes>
+        <Suspense
+          fallback={
+            <div className="text-center align-middle">
+              <Spinner animation="border" variant="secondary" size="lg" />
+            </div>
+          }
+        >
+          <div className="App container">
+            <div className="shadow bg-body rounded pb-5">
+              <div className="p-0">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/reminder" element={<ReminderPage />} />
+                  <Route
+                    path="/passwordReset"
+                    element={<ResetPasswordPage />}
+                  />
+                  <Route path="/message" element={<MessagePage />} />
+                  <Route path="/add_score" element={<AddScorePage />} />
+                  <Route path="/add_penalty" element={<AddPenaltyPage />} />
+                  <Route path="/event" element={<StageScorePage />} />
+                  <Route path="/joinToEvent" element={<TeamCreatePage />} />
+                  <Route path="/teamPanel" element={<TeamPanel />} />
+                </Routes>
+              </div>
             </div>
           </div>
-        </div>
-        <Footer />
+          <Footer />
+        </Suspense>
       </BrowserRouter>
     </div>
   );
