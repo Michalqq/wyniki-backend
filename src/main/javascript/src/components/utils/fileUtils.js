3e166e6a5c338337286e38a457cc94691f283b6d
Michał Kraciuk <michal.kraciuk@orange.com>
2022-05-03 23:05:48 +0200

    fix download file

--- src/main/javascript/src/components/utils/fileUtils.js
+++ src/main/javascript/src/components/utils/fileUtils.js
@@ -15,12 +15,24 @@ export function download(url, filename, handleResponse) {
 }
 
 export function openFile(file, filename) {
-  window.open(`data:application/${filename.split(".").pop()};base64, ` + file);
-  // var file = new Blob([file]);
-  // var urlFile = URL.createObjectURL(file);
-  // window.open(urlFile);
-  // var a = document.createElement("a");
-  //a.href = URL.createObjectURL(file);
-  //a.setAttribute("download", filename);
-  //a.click();
+  //window.open(`data:application/${filename.split(".").pop()};base64, ` + file);
+  var bytes = _base64ToArrayBuffer(file);
+  var link = new Blob([bytes], {
+    type: "application/" + filename.split(".").pop(),
+  });
+  var a = document.createElement("a");
+  a.href = URL.createObjectURL(link);
+  a.setAttribute("download", filename);
+  a.click();
+}
+
+//Base64 conversion
+function _base64ToArrayBuffer(base64) {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
 }
